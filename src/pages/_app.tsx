fc1d5a9e0f9655b1dfc8d5097e8a25e0b3cbe843
Martin Sione <martinsione03@gmail.com>
2022-02-21 20:06:35 -0300

    Move NotesContext to home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,6 @@
-import "../styles/globals.css";
-import { NotesProvider } from "@/context/Notes";
 import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <NotesProvider>
-      <Component {...pageProps} />
-    </NotesProvider>
-  );
+  return <Component {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
-import Sidebar from "@/components/Sidebar";
 import Editor from "@/components/Editor";
+import Sidebar from "@/components/Sidebar";
+import { NotesProvider } from "@/context/Notes";
 
 export default function Home() {
   return (
     <div className="h-screen w-screen flex">
-      <Sidebar />
-      <Editor />
+      <NotesProvider>
+        <Sidebar />
+        <Editor />
+      </NotesProvider>
     </div>
   );
 }
